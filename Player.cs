e558c86bfd0874aedbbdfc8e9a19190468315094
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2017-11-08 13:17:03 -0300

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -6,16 +6,29 @@ public class Player : MonoBehaviour {
 
 	public float speed;
 	public float laneSpeed;
+	public float jumpLength;
+	public float jumpHeight;
+	public float slideLength;
 
+	private Animator anim;
 	private Rigidbody rb;
+	private BoxCollider boxCollider;
 	private int currentLane = 1;
 	private Vector3 verticalTargetPosition;
+	private bool jumping = false;
+	private float jumpStart;
+	private bool sliding = false;
+	private float slideStart;
+	private Vector3 boxColliderSize;
 
 	// Use this for initialization
 	void Start () {
 
 		rb = GetComponent<Rigidbody>();
-
+		anim = GetComponentInChildren<Animator>();
+		boxCollider = GetComponent<BoxCollider>();
+		boxColliderSize = boxCollider.size;
+		anim.Play("runStart");
 	}
 	
 	// Update is called once per frame
@@ -28,6 +41,43 @@ public class Player : MonoBehaviour {
 		else if (Input.GetKeyDown(KeyCode.RightArrow))
 		{
 			ChangeLane(1);
+		}
+		else if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Jump();
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Slide();
+		}
+
+		if (jumping)
+		{
+			float ratio = (transform.position.z - jumpStart) / jumpLength;
+			if(ratio >= 1f)
+			{
+				jumping = false;
+				anim.SetBool("Jumping", false);
+			}
+			else
+			{
+				verticalTargetPosition.y = Mathf.Sin(ratio * Mathf.PI) * jumpHeight;
+			}
+		}
+		else
+		{
+			verticalTargetPosition.y = Mathf.MoveTowards(verticalTargetPosition.y, 0, 5 * Time.deltaTime);
+		}
+
+		if (sliding)
+		{
+			float ratio = (transform.position.z - slideStart) / slideLength;
+			if(ratio >= 1f)
+			{
+				sliding = false;
+				anim.SetBool("Sliding", false);
+				boxCollider.size = boxColliderSize;
+			}
 		}
 
 		Vector3 targetPosition = new Vector3(verticalTargetPosition.x, verticalTargetPosition.y, transform.position.z);
@@ -47,5 +97,31 @@ public class Player : MonoBehaviour {
 			return;
 		currentLane = targetLane;
 		verticalTargetPosition = new Vector3((currentLane - 1), 0, 0);
+	}
+
+	void Jump()
+	{
+		if (!jumping)
+		{
+			jumpStart = transform.position.z;
+			anim.SetFloat("JumpSpeed", speed / jumpLength);
+			anim.SetBool("Jumping", true);
+			jumping = true;
+		}
+	}
+
+	void Slide()
+	{
+		if(!jumping && !sliding)
+		{
+			slideStart = transform.position.z;
+			anim.SetFloat("JumpSpeed", speed / slideLength);
+			anim.SetBool("Sliding", true);
+			Vector3 newSize = boxCollider.size;
+			newSize.y = newSize.y / 2;
+			boxCollider.size = newSize;
+			sliding = true;
+
+		}
 	}
 }
