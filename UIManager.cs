639ee20549d9fda216eb4b70f045f2b892b856e6
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2017-11-22 11:03:59 -0200

    Add files via upload

---
+++ UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public Image[] lifeHearts;
+
+	public void UpdateLives(int lives)
+	{
+		for (int i = 0; i < lifeHearts.Length; i++)
+		{
+			if(lives > i)
+			{
+				lifeHearts[i].color = Color.white;
+			}
+			else
+			{
+				lifeHearts[i].color = Color.black;
+			}
+		}
+	}
+
+}
